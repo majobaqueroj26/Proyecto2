48433f420fc9883b1537a937987ac887a705164e
Maria Jose Baquero J <mj.baquero789@uniandes.edu.co>
2022-05-01 12:03:12 -0500

    Update Api

--- api.py
+++ api.py
@@ -1,45 +1,20 @@
 #!/usr/bin/python
-from flask import Flask
-from flask_restx import Api, Resource, fields
-import joblib
-from m09_model_deployment import predict_proba
+from flask import Flask, jsonify
+from sklearn.externals import joblib, 
+import pandas as pd
+import os
 
 app = Flask(__name__)
 
-api = Api(
-    app, 
-    version='1.0', 
-    title='Car Price Prediction API',
-    description='Car Price Prediction API')
+@app.route('/predict', methods=['POST'])
+def predict():
+     json_ = request.json
 
-ns = api.namespace('predict', 
-     description='Car Price Regressor')
-   
-parser = api.parser()
-
-parser.add_argument(
-    'URL', 
-    type=str, 
-    required=True, 
-    help='URL to be analyzed', 
-    location='args')
-
-resource_fields = api.model('Resource', {
-    'result': fields.String,
-})
-
-@ns.route('/')
-class CarPriceApi(Resource):
-
-    @api.doc(parser=parser)
-    @api.marshal_with(resource_fields)
-    def get(self):
-        args = parser.parse_args()
-        
-        return {
-         "result": predict_proba(args['URL'])
-        }, 200
-    
-    
+     query_df = pd.DataFrame(json_)
+     query = pd.get_dummies(query_df)
+     prediction = clf.predict(query)
+     return jsonify({'prediction': list(prediction)})
+     
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
+     clf = joblib.load(os.path.dirname(__file__) + '/NN_model.pkl')
+     app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
