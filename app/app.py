04b9b724775c571b05ac141b7b0bf610b43f885a
Maria Jose Baquero J <mj.baquero789@uniandes.edu.co>
2022-05-30 00:04:09 -0500

    Documentos API

--- app/app.py
+++ app/app.py
@@ -1,22 +1,34 @@
 import joblib
+import tensorflow as tf
+from keras.utils.data_utils import pad_sequences
 import numpy as np
 import json
 import pandas as pd
 
-model_file = '/opt/ml/model/NN_model.pkl'
-model = joblib.load(model_file)
-
+tokenizer_file = '/opt/ml/model/tokenizer.pkl'
+model_file = '/opt/ml/model/model-lstm.h5'
+tokenizer = joblib.load(tokenizer_file)
+model = tf.keras.models.load_model(model_file)
+clases = ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime',
+       'Documentary', 'Drama', 'Family', 'Fantasy', 'Film-Noir',
+       'History', 'Horror', 'Music', 'Musical', 'Mystery', 'News',
+       'Romance', 'Sci-Fi', 'Short', 'Sport', 'Thriller', 'War',
+       'Western']
 
 def lambda_handler(event, context):
     body = event['body']
     body = json.loads(body)
-    df = pd.DataFrame(body)
-    prediction = list(model.predict(df))
+    txt = str(body)
+    seq = tokenizer.texts_to_sequences([txt])
+    padded = pad_sequences(seq, maxlen=200)
+    prediction = model.predict(padded)
+    prediction = list(prediction[0])
+    index = [prediction.index(m) for m in prediction if m >= 0.40]
+    response = [clases[i] for i in index]
+    # df = pd.DataFrame(body)
+
+    #prediction = list(model.predict(df))
     return {
         'statusCode': 200,
-        'body': json.dumps(
-            {
-                "prediction": prediction,
-            }
-        )
+        'body': json.dumps({'clases': response}),
    }
